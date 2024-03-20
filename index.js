7221ae08aa07aa6ab9ec2fb5925f1ea63e61ee53
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-21 00:05:57 +0300

    review using arrows

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide(x, y) {
+    const divide = (x, y);
+    return x/y;
+    }
+console.log(2000, 100);
+const square = (a) => {
+    return a*a;
+}
+console.log(square(2));
+const add = (b, c) => {
+    return b + c;
+    }
+console.log(add(5, 2));
